1cafca06b45e960b40a84468bbd40dcdfa12a7c8
Nazar Dobrovolskyi <yfpfh233@gmail.com>
2024-10-22 00:23:54 +0300

    Update server node to create a topic relationship_logs

--- src/toxic_server/toxic_server/toxic_relationship_action_server.py
+++ src/toxic_server/toxic_server/toxic_relationship_action_server.py
@@ -4,6 +4,7 @@ import random
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
+from std_msgs.msg import String
 
 from toxic_relationship.action import ToxicRelationship
 
@@ -12,34 +13,51 @@ class ToxicRelationshipActionServer(Node):
 
     def __init__(self):
         super().__init__('toxic_relationship_action_server')
+
         self._action_server = ActionServer(
             self,
             ToxicRelationship,
             'relationship',
             self.execute_callback)
 
+
+        self.log_publisher = self.create_publisher(String, 'relationship_logs', 10)
+
+    def publish_log(self, message):
+        log_msg = String()
+        log_msg.data = message
+        self.log_publisher.publish(log_msg)
+
     def execute_callback(self, goal_handle):
-        self.get_logger().info(f'Recieve {goal_handle.request.toxicity_rate}% of toxicity rate from client')
-        self.get_logger().info('Process...')
+        toxicity_rate = goal_handle.request.toxicity_rate
+
+        self.get_logger().info(f'Received {toxicity_rate}% of toxicity rate from client')
+        self.publish_log(f'Received {toxicity_rate}% of toxicity rate from client')
+
+        self.get_logger().info('Processing...')
+        self.publish_log('Processing...')
 
         feedback_msg = ToxicRelationship.Feedback()
         years_alive = 0
         living_status = True
 
-        if goal_handle.request.toxicity_rate > 50:
-            feedback_msg.feedback = "I've almost sure I got a bad news for you 💔"
+        if toxicity_rate > 50:
+            feedback_msg.feedback = "I've almost sure I got bad news for you 💔"
             goal_handle.publish_feedback(feedback_msg)
+            self.publish_log("Feedback: Bad news incoming 💔")
 
         for i in range(1, 11):
             random_percentage_int = random.randint(1, 100)
             years_alive = i
-            if goal_handle.request.toxicity_rate > random_percentage_int:
+            if toxicity_rate > random_percentage_int:
                 living_status = False
                 self.get_logger().info('Client died 💀')
+                self.publish_log(f'Client died after {i} year(s) 💀')
                 break
             feedback_msg.feedback = f'You survived {i} year(s)'
             goal_handle.publish_feedback(feedback_msg)
             self.get_logger().info(f'{i}-st year of relationship')
+            self.publish_log(f'{i}-st year of relationship')
             time.sleep(1)
 
         goal_handle.succeed()
